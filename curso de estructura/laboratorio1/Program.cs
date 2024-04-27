5ee0542d54a72101231a84f8c00efe4523fa2fa9
yeymyloson18 <jeimy121loson@gmail.com>
2024-04-27 11:18:33 -0500

    cambio

--- curso de estructura/laboratorio1/Program.cs
+++ curso de estructura/laboratorio1/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, python!");
+
+// declaracion de variables
+
+//Console.WriteLine("ingrese un numero entero: ");
+//var numeroresivido = Console.ReadLine();
+//var numero = int.Parse(numeroresivido);
+//var esPar = false;
+
+//// resolucion
+
+//if (numero % 2 == 0)
+//{
+//    esPar = true;
+//}
+//// mostrar resultado
+
+
+//declaracion  y asignacion de las variables
+
+Console.WriteLine("ingrese un numero entero: ");
+var numeroresivido = Console.ReadLine();
+var numero = int.Parse(numeroresivido);
+Console.WriteLine("ingrese un numero entero: ");
+var numeroresivido = Console.ReadLine();
+var numero = int.Parse(numeroresivido);
+
+
+//resolucion del algoritmo
+
+
