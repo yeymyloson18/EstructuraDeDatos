3b385b750d557bdd180f5d7da81638c846d15701
yeymyloson18 <jeimy121loson@gmail.com>
2024-05-02 11:14:23 -0500

    cambio final

--- curso de estructura/laboratorio1/Program.cs
+++ curso de estructura/laboratorio1/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
 // declaracion de variables
 
 //Console.WriteLine("ingrese un numero entero: ");
@@ -16,16 +15,274 @@
 //// mostrar resultado
 
 
-//declaracion  y asignacion de las variables
+/*
+using System;
 
-Console.WriteLine("ingrese un numero entero: ");
-var numeroresivido = Console.ReadLine();
-var numero = int.Parse(numeroresivido);
-Console.WriteLine("ingrese un numero entero: ");
-var numeroresivido = Console.ReadLine();
-var numero = int.Parse(numeroresivido);
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            // Mostrar opciones al usuario
+            Console.WriteLine("Elija una operación:");
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicación");
+            Console.WriteLine("4. División");
+            Console.WriteLine("5. Salir");
+            Console.Write("Opción: ");
+
+            // Leer la opción del usuario
+            string opcion = Console.ReadLine();
+
+            // Salir si el usuario elige la opción 5
+            if (opcion == "5")
+            {
+                break;
+            }
+
+            // Solicitar al usuario que ingrese dos números
+            Console.Write("Ingrese el primer número: ");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese el segundo número: ");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            // Realizar la operación seleccionada
+            double resultado = 0;
+            switch (opcion)
+            {
+                case "1":
+                    resultado = numero1 + numero2;
+                    break;
+                case "2":
+                    resultado = numero1 - numero2;
+                    break;
+                case "3":
+                    resultado = numero1 * numero2;
+                    break;
+                case "4":
+                    if (numero2 != 0)
+                    {
+                        resultado = numero1 / numero2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se puede dividir entre cero.");
+                        continue;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Por favor, seleccione una opción válida.");
+                    continue;
+            }
+
+            // Mostrar el resultado de la operación
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+    }
+}
 
 
-//resolucion del algoritmo
+*/
+
+
+
+// mi primer hola mundo
+
+
+/*
+Console.WriteLine("este es el culso de estructura de datos !");
+Console.WriteLine("unsch 2024 !");
+Console.WriteLine("bola bolita");
+
+// Definir dos números para sumar
+int numero1 = 5;
+int numero2 = 7;
+
+// Sumar los dos números
+int suma = numero1 + numero2;
+
+// Mostrar el resultado
+Consol.WriteLine("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+
+
+int[] numeros = new int[10];
+
+numeros[0] = 1; numeros[1] = 2;
+numeros[2] = 3; numeros[3] = 4;
+numeros[4] = 5; numeros[5] = 6;
+numeros[6] = 7; numeros[7] = 8;
+numeros[8] = 9; numeros[9] = 0;
+
+var numero = numeros[3];
+var cantidad = numeros.Length;
+*/
+
+
+
+/*se desea calcular la distancia recorrida (m) por un móvil que tiene velocidad constante
+(m/s) durante un tiempo T (sg), considerar que es un MRU (movimiento rectilíneo uniforme) 
+
+
+double d, v, t;
+Console.Write("ingrese la velocidad constante: ");
+v = int.Parse(Console.ReadLine());
+Console.Write("ingrese el tiempo : "); 
+t = int.Parse(Console.ReadLine());
+d = v * t;
+Console.Write("la distancia es: {0}", d); Console.ReadLine();
+
+*/
+
+
+
+/*
+int Factorial(int fact)
+{
+    if (fact > 0)
+{
+        int valor = fact * Factorial(fact - 1);
+        return valor;
+    }
+else
+        return 1;
+}
+
+int f = Factorial(5);
+Console.WriteLine(" El factorial de 5 es " +f);
+Console.ReadKey();
+
+*/
+
+
+
+//int Fibonacci(int n)
+//{
+//    if ((n == 0) || (n == 1))
+//    {
+//        return n;
+//    }
+//    else
+
+//    {
+//        return Fibonacci(n - 1) + Fibonacci(n - 2);
+//    }
+//}
+//Console.Write(" ESCRIBA LA CANTIDAD DE LOS NUMEROS DE FIBONACCI QUE DESEA IMPRIMIR: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+//for (int i = 2; i <= n + 1; i++)
+//{
+//    Console.WriteLine(Fibonacci(i));
+//}
+
+
+
+//// Declarar a array simple de 5 enteros.
+//int[] array1 = new int[5];
+//// Declarar y asignar array de enteros.
+//int[] array2 = [1, 2, 3, 4, 5, 6];
+//// Declarar un array de 2 dimensiones.
+//int[,] multiDimensionalArray1 = new int[2, 3];
+//// Declarar y asignar un array 2 dimensiones de enteros.
+//int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+
+//using System.Text;
+
+//int[] array = new int[5];
+//string[] weekDays = ["Lunes ;", "Martes", " Miercoles ", " Jueves ", " Viernes", " Sabado","Domingo"];
+//Console.WriteLine(weekDays[0]);
+//Console.WriteLine(weekDays[1]);
+//Console.WriteLine(weekDays[2]);
+//Console.WriteLine(weekDays[3]);
+//Console.WriteLine(weekDays[4]);
+//Console.WriteLine(weekDays[5]);
+//Console.WriteLine(weekDays[6]);
+
+
+
+
+//Console.Write(" Ingrese tamaño del array : ");
+//int n = int.Parse(Console.ReadLine());
+//int[] m = new int[n];
+//Console.WriteLine("Ingrese elementos : ");
+//bool multiplo = false; int temp = 0;
+//for (int i = 0; i < m.Length; i++)
+//{
+//    multiplo = false;
+//    while (multiplo == false)
+//    {
+//        Console.Write("Ingrese elemento ["+ i + "]:");
+//        temp = int.Parse(Console.ReadLine()) ;
+//        if (temp % 5 == 0)
+//        { m[i] = temp; multiplo = true; }
+//        else
+//        { Console.WriteLine(temp + " no es múltiplo de 5 "); }
+//    }
+//}
+//Console.WriteLine("Los elementos del array son :");
+//foreach (int i in m)
+//{ Console.Write(i + " "); }
+//Console.ReadLine();
+
+
+Console.Write("Ingrese el tamaño del arreglo: ");
+int tamaño = int.Parse(Console.ReadLine());
+
+int[] arreglo = new int[tamaño];
+
+for (int i = 0; i < tamaño; i++)
+{
+    Console.Write("Ingrese el elemento " + (i + 1) + ": ");
+    arreglo[i] = int.Parse(Console.ReadLine());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- lavoratorio2/labo2/Program.cs
+++ lavoratorio2/labo2/Program.cs
@@ -1,6 +1,423 @@
-﻿// mi primer hola mundo
+﻿
 
 
-Console.WriteLine("este es el culso de estructura de datos !");
-Console.WriteLine("unsch 2024 !");
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1) Realiza la suma, resta, multiplicación y división de dos números ingresados por el usuario.
+
+
+/* 
+
+
+double numero1, numero2, suma, resta, multiplicacion, division;
+
+Console.Write("Ingrese el primer número: ");
+numero1 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo número: ");
+numero2 = int.Parse(Console.ReadLine());
+
+suma = numero1 + numero2;
+resta = numero1 - numero2;
+multiplicacion = numero1 * numero2;
+division = numero1 / numero2;
+
+Console.WriteLine("Suma: " + suma);
+Console.WriteLine("Resta: " + resta);
+Console.WriteLine("Multiplicación: " + multiplicacion);
+Console.WriteLine("División: " + division);   */
+
+
+
+
+//2) Solicita un número al usuario y determina si es par o impar.
+
+
+/*
+double numero;
+Console.Write("Ingrese un número: ");
+numero = double.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("El número " + numero + " es par");
+}
+else
+{
+    Console.WriteLine("El número " + numero + " es impar");
+}
+
+*/
+/*
+double numero;
+Console.Write("Ingrese un número: ");
+numero = double.Parse(Console.ReadLine());
+
+string mensaje = (numero % 2 == 0) ? "El número " + numero + " es par" : "El número " + numero + " es impar";
+Console.WriteLine(mensaje);
+*/
+
+
+// 3) Pide la base y la altura de un triángulo al usuario y calcula su área. 
+
+/*  
+
+
+double bace, altura, area;
+
+Console.Write("ingrese la medida de la base del triangulo: ");
+bace = double.Parse(Console.ReadLine());
+
+Console.Write("\"ingrese la medida de la altura del triangulo: ");
+altura = double.Parse(Console.ReadLine());
+
+area = (bace * altura) / 2;
+Console.WriteLine("El área del triángulo es: " + area);
+
+*/
+
+
+// 4) Crea una función que calcule la factorial de un número. 
+
+/*
+
+
+Console.Write("Ingrese un número para calcular su factorial: ");
+int numero = int.Parse(Console.ReadLine());
+
+long factorial = 1;
+for (int i = 1; i <= numero; i++)
+{
+    factorial *= i;
+}
+
+Console.WriteLine("El factorial de " + numero + " es: " + factorial);
+
+
+*/
+
+//5) Verifica si un número ingresado por el usuario es primo o no.
+
+
+/*
+
+
+Console.Write("Ingrese un número: ");
+int numero = int.Parse(Console.ReadLine());
+int divisor = 1, contador = 0;
+
+while (divisor <= numero)
+{
+    if (numero % divisor == 0)
+    {
+        contador++;
+    }
+    divisor++;
+}
+
+if (contador == 2)
+{
+    Console.WriteLine("El número es primo.");
+}
+else
+{
+    Console.WriteLine("El número no es primo.");
+}
+
+
+*/
+
+
+//6) Toma una cadena de texto y muestra su inversión. 
+
+/*
+
+
+Console.Write("Ingrese una cadena de texto: ");
+string texto = Console.ReadLine();
+
+string textoInvertido = "";
+
+for (int i = texto.Length - 1; i >= 0; i--)
+{
+    textoInvertido += texto[i];
+}
+
+Console.WriteLine("La cadena invertida es: " + textoInvertido);
+
+
+*/
+
+
+//7) Calcula la suma de los números pares en un rango especificado por el usuario. 
+
+
+/*
+
+
+Console.Write("Ingrese el inicio del rango: ");
+int inicio = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el fin del rango: ");
+int fin = int.Parse(Console.ReadLine());
+
+int sumaPares = 0;
+for (int i = inicio; i <= fin; i++)
+{
+    if (i % 2 == 0)
+    {
+        sumaPares += i;
+    }
+}
+
+Console.WriteLine("La suma de los números pares en el rango es: " + sumaPares);
+
+*/
+
+
+//8) Crea una lista de los cuadrados de los primeros 10 números naturales. 
+
+
+/*
+// Crear una lista para almacenar los cuadrados de los números naturales
+List<int> cuadrados = new List<int>();
+
+// Calcular y agregar los cuadrados de los primeros 10 números naturales a la lista
+for (int i = 1; i <= 10; i++)
+{
+    int cuadrado = i * i;
+    cuadrados.Add(cuadrado);
+}
+
+// Mostrar la lista de cuadrados
+Console.WriteLine("Los cuadrados de los primeros 10 números naturales son:");
+foreach (int cuadrado in cuadrados)
+{
+    Console.WriteLine(cuadrado);
+}
+
+*/
+
+
+//9) Cuenta el número de vocales en una cadena de texto. 
+
+/*
+
+// Solicitar al usuario que ingrese una cadena de texto
+        Console.Write("Ingrese una cadena de texto: ");
+string texto = Console.ReadLine().ToLower(); // Convertir la cadena a minúsculas para simplificar la comparación
+
+// Inicializar el contador de vocales
+int contadorVocales = 0;
+
+// Iterar a través de cada carácter de la cadena y contar las vocales
+foreach (char caracter in texto)
+{
+    // Verificar si el carácter es una vocal
+    if (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u')
+    {
+        contadorVocales++;
+    }
+}
+
+// Mostrar el número de vocales en la cadena
+Console.WriteLine("El número de vocales en la cadena es: " + contadorVocales);
+
+*/
+
+
+//10) Genera los primeros 10 números de la serie Fibonacci. 
+
+
+/*
+// Definir los primeros dos números de la serie Fibonacci
+int a = 0;
+int b = 1;
+
+// Mostrar los primeros dos números
+Console.WriteLine("Los primeros 10 números de la serie Fibonacci son:");
+Console.Write(a + " " + b + " ");
+
+// Generar los siguientes 8 números de la serie Fibonacci
+for (int i = 2; i < 10; i++)
+{
+    int c = a + b; // Calcular el siguiente número de la serie sumando los dos anteriores
+    Console.Write(c + " ");
+
+    // Actualizar los valores de a y b para la siguiente iteración
+    a = b;
+    b = c;
+}
+
+
+*/
+
+
+
+
+
+//11) Ordena una lista de números ingresados por el usuario de menor a mayor. 
+
+
+/*
+
+
+// Solicitar al usuario que ingrese la cantidad de números
+Console.Write("Ingrese la cantidad de números: ");
+int cantidad = int.Parse(Console.ReadLine());
+
+// Crear un arreglo para almacenar los números ingresados por el usuario
+int[] numeros = new int[cantidad];
+
+// Solicitar al usuario que ingrese los números y almacenarlos en el arreglo
+for (int i = 0; i < cantidad; i++)
+{
+    Console.Write("Ingrese el número " + (i + 1) + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+} 
+
+// Ordenar los números de menor a mayor
+Array.Sort(numeros);
+
+// Mostrar los números ordenados
+Console.WriteLine("Los números ordenados de menor a mayor son:");
+foreach (int numero in numeros)
+{
+    Console.Write(numero + " ");
+}
+
+*/
+
+
+//12) Verifica si una palabra ingresada por el usuario es un palíndromo. 
+
+
+/*
+ 
+
+// Solicitar al usuario que ingrese una palabra
+Console.Write("Ingrese una palabra: ");
+string palabra = Console.ReadLine().ToLower(); // Convertir la palabra a minúsculas para simplificar la comparación
+
+// Verificar si la palabra es un palíndromo
+bool esPalindromo = true;
+for (int i = 0; i < palabra.Length / 2; i++)
+{
+    // Comparar el i-ésimo carácter con el carácter correspondiente desde el final
+    if (palabra[i] != palabra[palabra.Length - i - 1])
+    {
+        esPalindromo = false;
+        break; // Si no coinciden, la palabra no es un palíndromo, se detiene el bucle
+    }
+}
+
+// Mostrar el resultado
+if (esPalindromo)
+{
+    Console.WriteLine("La palabra \"" + palabra + "\" es un palíndromo.");
+}
+else
+{
+    Console.WriteLine("La palabra \"" + palabra + "\" no es un palíndromo.");
+}
+
+*/
+
+
+// 13) Crea un programa que genere la tabla de multiplicar de un número ingresado por el usuario. 
+
+
+/*
+// Solicitar al usuario que ingrese un número para generar su tabla de multiplicar
+Console.Write("Ingrese un número para generar su tabla de multiplicar: ");
+int numero = int.Parse(Console.ReadLine());
+
+// Mostrar la tabla de multiplicar del número ingresado
+Console.WriteLine("Tabla de multiplicar del número " + numero + ":");
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+}
+*/
+
+
+//14) Pide el radio de un círculo al usuario y calcula su área. 
+
+/*
+// Solicitar al usuario que ingrese el radio del círculo
+        Console.Write("Ingrese el radio del círculo: ");
+        double radio = double.Parse(Console.ReadLine());
+
+        // Calcular el área del círculo utilizando la fórmula: área = π * radio^2
+        double area = Math.PI * Math.Pow(radio, 2);
+
+        // Mostrar el área del círculo
+        Console.WriteLine("El área del círculo con radio " + radio + " es: " + area);
+ 
+*/
+
+
+// 15) Toma un número entero y calcula la suma de sus dígitos. 
+
+/*
+
+// Solicitar al usuario que ingrese un número entero
+Console.Write("Ingrese un número entero: ");
+int numero = int.Parse(Console.ReadLine());
+
+// Calcular la suma de los dígitos del número
+int sumaDigitos = 0;
+int numeroAbsoluto = Math.Abs(numero); // Tomar el valor absoluto para manejar números negativos
+while (numeroAbsoluto > 0)
+{
+    sumaDigitos += numeroAbsoluto % 10; // Obtener el último dígito y sumarlo a la suma total
+    numeroAbsoluto /= 10; // Eliminar el último dígito del número
+}
+
+// Mostrar la suma de los dígitos
+Console.WriteLine("La suma de los dígitos del número es: " + sumaDigitos);
+ 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
